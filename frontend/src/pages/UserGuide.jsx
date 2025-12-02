4dc24cb305940c99378de8fdb3a8b8f0cc112357
Mmabatho Thathana <mmabathomabiiey@gmail.com>
2025-12-02 16:26:43 +0200

    Added the medical facilities page

---
+++ frontend/src/pages/UserGuide.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar"
+export default function Privacy() {
+  return (
+    <div style={{ paddingTop: "60px" }}>
+      <Navbar />
+      <h1>How to use</h1><br />
+   </div>
+  )
+}
